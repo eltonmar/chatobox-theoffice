896761fd6061f11f8dcb663d2e227502d31615cf
Elton Marinho <elton.marinho97@gmail.com>
2022-04-01 17:27:40 -0300

    Doc

---
+++ docs/static/NQPUDJw1sO9goCMR5EUzJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-46253fbc6277723c.js","static/chunks/497-775cf45e02937e8f.js","static/chunks/pages/index-35fc4c535236cc22.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
